ce2dfe0bad91c048a865df08578888397fd02a71
jyoseizen <142558281+jyoseizen@users.noreply.github.com>
2024-03-19 22:29:17 +0800

    Update Text .java（逆猜数游戏）
    逆猜数游戏

--- Text .java
+++ Text .java
@@ -1,24 +1,30 @@
 import java.util.*;
 
-public class Text{
+public class Test{
     public static void main(String[] args){
-        System.out.println("请输入一个你心仪的数字"\n"这个数字要在0~100之间");
+        System.out.println("请在下面输入一个你的幸运数字");
         Scanner sc = new Scanner(System.in);
-        int targetnumber = sc.nextInt();//目标数字
-	
-	Random r = new Random();
-	int guessnumber = r.nextInt(101);
-	while(guessnumber != targetnumber){
-		int temp = guessnumber;
-		if (guessnumber > targetnumber){
-			System.out.println("看来"+guessnumber+”这个数字过大了，我再猜猜“);
-			guessnumber = r.nextInt(guessnumber);
-			}
-		else (guessnumber < targetnumber){
-			System.out.println("看来"+guessnumber+”这个数字过大了，我再猜猜“);
-			guessnumber = r.nextInt(guessnumber);
-			}
-        }
-	System.out.println(guessnumber+"这就是你想要的数字吧");
+        int targetNumber = sc.nextInt();
+        int numDigits = String.valueOf(targetNumber).length();
+	    Random r = new Random();
+        int min = 0;
+        int max = (int) Math.pow(10, numDigits);
+        int guessNumber = r.nextInt(max - min ) + min;
+        int guesses = 1;
+	    while(guessNumber != targetNumber){
+		    if (guessNumber > targetNumber){
+			    System.out.println("看来"+guessNumber+"这个数字过大了，我再猜猜");     
+                max = guessNumber;
+			    }
+		    else if(guessNumber < targetNumber){
+			    System.out.println("看来"+guessNumber+"这个数字过小了，我再猜猜");
+                min = guessNumber;
+			    }
+            guessNumber = r.nextInt(max - min ) + min;
+            guesses++;
+         }
+
+	    System.out.println(guessNumber+"这就是你想要的数字吧");
+        System.out.println("猜测次数为：" + guesses);
     }
 }
